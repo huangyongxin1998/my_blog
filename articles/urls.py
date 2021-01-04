a499c2b12f21a668c26c9412d689f0495b55cfac
黄永欣 <1714120359@qq.com>
2021-01-04 11:06:30 +0800

    'settings设置完善'

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from articles import views
 
 urlpatterns=[
-    path('list/',views.index,name='list'),
+    path('index/',views.index,name='index'),
     path('detail/<int:pk>',views.detail,name='detail'),
-    path('concate/',views.concate,name='concate'),
+    path('contact/',views.contact,name='contact'),
     path('about/',views.about,name='about'),
 ]
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'staic')
 )
+
+"python.linting.pylintArgs": [
+    "--errors-only",
+    "--disable=E0401"
+],
